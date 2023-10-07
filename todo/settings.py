ae391c84f484eecd769001fc0dd5339b2d6af79d
Akshay N <akshay.n@stabilixindia.biz>
2023-10-07 23:27:28 +0530

    add crispy

--- todo/settings.py
+++ todo/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'task',
     'crispy_forms',
-    'crispy_bootstrap5',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -131,5 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
